d3209ccc4ebe6cc3c4f8de0106d3fc3af45c2286
Toni3101 <antonioha3101@gmail.com>
2022-11-25 11:32:23 +0100

    try3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const {connect} = require('./utils/database');
-const dotenv = require('dotenv');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const {connect} = require('./utils/database');
+
+
+const cloudinary = require('cloudinary').v2;
 
 
 const productsRouter = require('./api/routes/products.routes');
@@ -11,12 +14,24 @@ const sosRouter = require('./api/routes/sos.routes');
 const diaryRouter = require('./api/routes/diary.routes');
 
 dotenv.config();
-
 const PORT = process.env.PORT || 8000;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET
+});
 
 
 const app = express();
 connect();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Method', 'POST, GET, DELETE, PUT, PATCH');
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 app.use(cors({
   origin: '*',
@@ -31,6 +46,14 @@ app.use('/products', productsRouter);
 app.use('/allergens', allergensRouter);
 app.use('/sos', sosRouter);
 app.use('/diary', diaryRouter);
+
+app.use('*', (req,res,next) => {
+  return res.status(404).json("Route not found")
+});
+
+app.use((error, req, res, next) => {
+  return res.status( error.status || 500 ).json("Error: " + error.message || "Unexpected error");
+});
 
 
 app.listen(PORT, () => console.log(`listening on port:http://localhost:${PORT}`));
